130361a8abba5033f9d090154c65298a7c716e39
Ktsierra <josejaviersierra1998@gmail.com>
2025-07-01 10:49:50 -0400

    refactor memory_pressure script: simplify output format by removing "Pressure:" prefix

--- scripts/memory_pressure.sh
+++ scripts/memory_pressure.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-memory_pressure | awk '/System-wide memory free percentage:/ {printf "Pressure: %d%%", 100 - $5}'
+memory_pressure | awk '/System-wide memory free percentage:/ {printf "%d%%", 100 - $5}'
